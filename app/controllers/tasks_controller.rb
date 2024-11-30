cf0c0a5cbe6d906ede0defaf62389c58b2d79b39
Masashi Takahashi <0xv17851726325v@gmail.com>
2024-11-30 23:47:28 +0900

    rubocop指摘修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: %i[show edit update destroy]
+
   def index
   end
 
